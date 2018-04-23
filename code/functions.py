7b178cc88c95766943238d62800a4e80f06f8294
Amy van der Gun <amyvdgun@live.nl>
2018-04-23 15:54:51 +0200

    minor fix

--- code/functions.py
+++ code/functions.py
@@ -7,11 +7,8 @@ def createGrid(gridXlength, gridYlength):
     # create empty grid and initialize dot
     grid = []
     dot = 0
-<<<<<<< HEAD
 
     # iterate Xlength times and add empty array
-=======
->>>>>>> 9d046621ad94e6177574b168b2bba609570570be
     for i in range(gridXlength):
         row = []
         grid.append(row)
@@ -22,13 +19,9 @@ def createGrid(gridXlength, gridYlength):
 
     # grid[0][0] is linksboven
     # grid[Y]grid[X] scheme
-<<<<<<< HEAD
-    grid[randint(0,15)][randint(0,17)] = 1
+    grid[randint(0,17)][randint(0,15)] = 1
     # grid[randint(0,15)][randint(0,17)] = 2
     # grid[randint(0,15)][randint(0,17)] = 3
-=======
-    grid[5][1] = "A"
->>>>>>> 9d046621ad94e6177574b168b2bba609570570be
     for element in grid:
         print(element)
 
