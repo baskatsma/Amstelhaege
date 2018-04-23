f6b20c4670fccaa21c29b79606b8d22065867ab8
Bas Katsma <bas.katsma@student.uva.nl>
2018-04-23 15:18:27 +0200

    Comments

--- code/functions.py
+++ code/functions.py
@@ -3,11 +3,14 @@ def createGrid(gridXlength, gridYlength):
 
     grid = []
     dot = "."
-    for i in range(gridYlength):
+    for i in range(gridXlength):
         row = []
         grid.append(row)
-        for j in range(gridXlength):
+        for j in range(gridYlength):
             row.append(dot)
 
+    # grid[0][0] is linksboven
+    # grid[Y]grid[X] scheme
+    grid[5][1] = "A"
     for element in grid:
         print(element)
