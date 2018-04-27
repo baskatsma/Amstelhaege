5647db6d5a8ae0a0bfd8bdb41baea0afab337e5d
Bas Katsma <bas.katsma@student.uva.nl>
2018-04-27 17:38:55 +0200

    Re-wrote House class and re-wrote house initialization process

--- code/models/models.py
+++ code/models/models.py
@@ -10,17 +10,27 @@ USAGE       to-do
 # %%
 from random import randint
 
-# Initialize genericHouse class
-class genericHouse:
+class House(object):
     """
-    Generic House class that contains functions that all the subclasses use
+    Contains all house properties and functions
     """
 
-    def __init__(self):
-        print("genericHouse is made.")
+    def __init__(self, type, houseDimensions, freeArea, extraFreeArea, value, valueIncrease, positionX, positionY, gridXLength, gridYLength, uniqueID):
+        self.type = type
+        self.houseDimensions = houseDimensions
+        self.freeArea = freeArea
+        self.extraFreeArea = extraFreeArea
+        self.value = value
+        self.valueIncrease = valueIncrease
+        self.positionX = positionX
+        self.positionY = positionY
+        self.gridXLength = gridXLength
+        self.gridYLength = gridYLength
+        self.uniqueID = uniqueID
 
     # Calculates the new price of the property, based on the extra free area
     def calculateNewValue(self):
+        
         # Extra vrijstand * base percentage
         totalIncreasePercentage = self.extraFreeArea * self.valueIncrease
 
@@ -29,44 +39,8 @@ class genericHouse:
 
         return newHouseValue
 
-# Eengezinswoning
-class Eengezinswoning(genericHouse):
-    def __init__(self, gridXLength, gridYLength):
-        self.type = "eengezinswoning"
-        self.houseDimensions = (8, 8)       # (length, depth) tuple
-        self.freeArea = 2      # Vrijstand in meters
-        self.extraFreeArea = 5     # Extra vrijstand in meters
-        self.value = 285000
-        self.valueIncrease = float(0.03)
-        self.positionX = 0
-        self.positionY = 0
-
-# Bungalow
-class Bungalow(genericHouse):
-    def __init__(self, gridXLength, gridYLength):
-        self.type = "bungalow"
-        self.houseDimensions = (10, 7.5)        # (length, depth) tuple
-        self.freeArea = 3      # Vrijstand in meters
-        self.extraFreeArea = 0     # Extra vrijstand in meters
-        self.value = 399000
-        self.valueIncrease = float(0.04)
-        self.positionX = 0
-        self.positionY = 0
-
-# Maison
-class Maison(genericHouse):
-    def __init__(self, gridXLength, gridYLength):
-        self.type = "maison"
-        self.houseDimensions = (11, 10.5)       # (length, depth) tuple
-        self.freeArea = 6      # Vrijstand in meters
-        self.extraFreeArea = 0     # Extra vrijstand in meters
-        self.value = 610000
-        self.valueIncrease = float(0.06)
-        self.positionX = 0
-        self.positionY = 0
-
 # Initialize Grid class
-class Grid:
+class Grid(object):
     """
     Grid that contains all houses
     """
@@ -121,3 +95,65 @@ class Grid:
         grid[randint(-1,17)][randint(-1,15)] = eengezinswoning
         grid[randint(-1,17)][randint(-1,15)] = bungalow
         grid[randint(-1,17)][randint(-1,15)] = maison
+
+
+
+
+
+
+
+
+# # Initialize genericHouse class
+# class genericHouse:
+#     """
+#     Generic House class that contains functions that all the subclasses use
+#     """
+#
+#     def __init__(self):
+#         print("genericHouse is made.")
+#
+#     # Calculates the new price of the property, based on the extra free area
+#     def calculateNewValue(self):
+#         # Extra vrijstand * base percentage
+#         totalIncreasePercentage = self.extraFreeArea * self.valueIncrease
+#
+#         # Base price + (base price * total percentage)
+#         newHouseValue = self.value + (self.value * totalIncreasePercentage)
+#
+#         return newHouseValue
+#
+# # Eengezinswoning
+# class Eengezinswoning(genericHouse):
+#     def __init__(self, gridXLength, gridYLength):
+#         self.type = "eengezinswoning"
+#         self.houseDimensions = (8, 8)       # (length, depth) tuple
+#         self.freeArea = 2      # Vrijstand in meters
+#         self.extraFreeArea = 5     # Extra vrijstand in meters
+#         self.value = 285000
+#         self.valueIncrease = float(0.03)
+#         self.positionX = 0
+#         self.positionY = 0
+#
+# # Bungalow
+# class Bungalow(genericHouse):
+#     def __init__(self, gridXLength, gridYLength):
+#         self.type = "bungalow"
+#         self.houseDimensions = (10, 7.5)        # (length, depth) tuple
+#         self.freeArea = 3      # Vrijstand in meters
+#         self.extraFreeArea = 0     # Extra vrijstand in meters
+#         self.value = 399000
+#         self.valueIncrease = float(0.04)
+#         self.positionX = 0
+#         self.positionY = 0
+#
+# # Maison
+# class Maison(genericHouse):
+#     def __init__(self, gridXLength, gridYLength):
+#         self.type = "maison"
+#         self.houseDimensions = (11, 10.5)       # (length, depth) tuple
+#         self.freeArea = 6      # Vrijstand in meters
+#         self.extraFreeArea = 0     # Extra vrijstand in meters
+#         self.value = 610000
+#         self.valueIncrease = float(0.06)
+#         self.positionX = 0
+#         self.positionY = 0
--- code/test.py
+++ code/test.py
@@ -9,42 +9,91 @@ maxHouses = defineMaxHouses()
 gridXLength = 18
 gridYLength = 16
 
+# Define house templates (in dictionary format) to create class instances
+eengezinswoningTemplate = {"type": "eengezinswoning",
+                            "houseDimensions": (8, 8),
+                            "freeArea": 2,
+                            "extraFreeArea": 5,
+                            "value": 285000,
+                            "valueIncrease": float(0.03),
+                            "positionX": 0,
+                            "positionY": 0,
+                            "gridXLength": gridXLength,
+                            "gridYLength": gridYLength,
+                            "uniqueID": 0}
+
+bungalowTemplate = {"type": "bungalow",
+                    "houseDimensions": (10, 7.5),
+                    "freeArea": 3,
+                    "extraFreeArea": 0,
+                    "value": 399000,
+                    "valueIncrease": float(0.04),
+                    "positionX": 0,
+                    "positionY": 0,
+                    "gridXLength": gridXLength,
+                    "gridYLength": gridYLength,
+                    "uniqueID": 0}
+
+maisonTemplate = {"type": "maison",
+                    "houseDimensions": (11, 10.5),
+                    "freeArea": 6,
+                    "extraFreeArea": 0,
+                    "value": 610000,
+                    "valueIncrease": float(0.06),
+                    "positionX": 0,
+                    "positionY": 0,
+                    "gridXLength": gridXLength,
+                    "gridYLength": gridYLength,
+                    "uniqueID": 0}
+
 # Create grid instance
 gridField = Grid(gridXLength, gridYLength, maxHouses)
 gridArray = gridField.drawGrid()
 
-# %% Test houses
-a = Eengezinswoning(gridXLength, gridYLength)
-b = Bungalow(gridXLength, gridYLength)
-c = Maison(gridXLength, gridYLength)
-
 # %% Test woonwijk
 residentialArea = []
 
-# Out of the maxHouses houses, add 0.60 * maxHouses eengezinswoningen
+# Create new houses based on the grid requirements
 for eengezinswoning in range(gridField.totalAmountEengezinswoningen):
-    residentialArea.append(Eengezinswoning(gridXLength, gridYLength))
+    residentialArea.append(House(**eengezinswoningTemplate))
 
-# Out of the maxHouses houses, add 0.25 * maxHouses bungalows
 for bungalow in range(gridField.totalAmountBungalows):
-    residentialArea.append(Bungalow(gridXLength, gridYLength))
+    residentialArea.append(House(**bungalowTemplate))
 
-# Out of the maxHouses houses, add 0.15 * maxHouses maisons
 for maison in range(gridField.totalAmountMaisons):
-    residentialArea.append(Maison(gridXLength, gridYLength))
+    residentialArea.append(House(**maisonTemplate))
 
-# PRINT TESTS
-# 
-# print("maxHouses on grid is: " + str(gridField.maxHouses))
-# print("fractionEengezinswoningen is: " + str(gridField.fractionEengezinswoningen))
+# Update uniqueIDs
+for house in range(len(residentialArea)):
+    residentialArea[house].uniqueID = house
+
+# Print test woonwijk
+for i in range(len(residentialArea)):
+    print(residentialArea[i].type, "|| uniqueID is:", residentialArea[i].uniqueID)
+
+# Print test extra House functions
+b = House(**eengezinswoningTemplate)
+print("")
+print("B is een " + b.type + " met afmetingen:", b.houseDimensions)
+print("De waarde van B is: " + str(b.value) + " euro")
+print("B nieuwe waarde met extra vrijstand (", b.extraFreeArea, "meter ) is:", b.calculateNewValue())
+
+
+
+
+
+
+
+
+
+# # Out of the maxHouses houses, add 0.60 * maxHouses eengezinswoningen
+# for eengezinswoning in range(gridField.totalAmountEengezinswoningen):
+#     residentialArea.append(Eengezinswoning(gridXLength, gridYLength))
 #
-# print("A is een " + a.type + " met afmetingen:", a.houseDimensions)
-# print("De waarde van A is: " + str(a.value) + " euro")
-# print("A nieuwe waarde met extra vrijstand is:", a.calculateNewValue())
+# # Out of the maxHouses houses, add 0.25 * maxHouses bungalows
+# for bungalow in range(gridField.totalAmountBungalows):
+#     residentialArea.append(Bungalow(gridXLength, gridYLength))
 #
-# print("B is een " + b.type + " met afmetingen:", b.houseDimensions)
-# print("De waarde van B is: " + str(b.value) + " euro")
-# print("B nieuwe waarde met extra vrijstand is:", b.calculateNewValue())
-#
-# for i in range(len(residentialArea)):
-#     print(residentialArea[i].type)
+# # Out of the maxHouses houses, add 0.15 * maxHouses maisons
+# for maison in range(gridField.totalAmountMaisons):
+#     residentialArea.append(Maison(gridXLength, gridYLength))
